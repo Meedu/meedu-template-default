4e9029e3e85d7e235818721485d903f534a4b808
小滕 <xiaoteng@192.168.0.103>
2020-10-17 15:31:55 +0800

    登录界面美化

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,23 +1,106 @@
 <style lang="less" scoped>
-.login-box {
+.main-body {
   width: 100%;
+  height: 100vh;
+  background: url("~assets/images/bg.jpg") no-repeat;
+  background-size: cover;
+}
+
+.logo-box {
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  margin-left: -70px;
+  width: 140px;
+  height: 37px;
+}
+
+.login-box {
+  position: absolute;
+  top: 150px;
+  left: 50%;
+  margin-left: -200px;
+  z-index: 11;
+  width: 400px;
   height: auto;
-  float: left;
-  margin-top: 100px;
   padding: 30px 30px 20px 30px;
   background-color: white;
   border-radius: 10px;
   box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.05);
 
-  .title {
+  .tabs {
     width: 100%;
     height: auto;
     float: left;
+    border-bottom: 1px solid #eeeeee;
+    margin-bottom: 45px;
+
+    .tab-item {
+      width: 64px;
+      height: auto;
+      float: left;
+      color: rgb(153, 153, 153);
+      line-height: 24px;
+      font-weight: 500;
+      margin-right: 30px;
+      padding-bottom: 20px;
+      margin-bottom: -1px;
+      cursor: pointer;
+
+      &.active,
+      &:hover {
+        color: #282828;
+        border-bottom: 2px solid #282828;
+      }
+    }
+  }
+
+  .input-form-item {
+    width: 100%;
+    height: 42px;
+    float: left;
+    padding-left: 20px;
+    padding-right: 20px;
+    color: #666666;
+    font-size: 14px;
+    border: 1px solid #dddddd;
+    background-color: #f4f4f4;
+    border-radius: 4px;
+    margin-bottom: 20px;
+
+    &:focus {
+      background-color: white;
+      outline: none;
+    }
+  }
+
+  .auth-button,
+  .send-sms-button {
+    width: 100%;
+    height: 42px;
+    float: left;
+    margin-bottom: 20px;
+    font-size: 16px;
+    border: 1px solid #eeeeee;
     text-align: center;
-    margin-bottom: 30px;
-    font-size: 20px;
-    color: #000;
-    font-weight: 600;
+    outline: none;
+    background-color: #eeeeee;
+    color: #bbbbbb;
+    border-radius: 4px;
+
+    &.active {
+      color: white;
+      background-color: rgba(40, 177, 138, 1);
+      border: 1px solid rgba(50, 180, 138, 1);
+
+      &:hover {
+        background-color: rgba(40, 177, 138, 0.8);
+      }
+    }
+  }
+
+  .send-sms-button {
+    width: 120px;
   }
 
   .sms-login-button {
@@ -35,140 +118,191 @@
     width: 100%;
     height: auto;
     float: left;
+    margin-bottom: 0;
+  }
 
-    label {
+  .other-buttons {
+    width: 100%;
+    height: auto;
+    float: left;
+    margin-bottom: 30px;
+    text-align: right;
+    color: #999999;
+    font-size: 14px;
+
+    a {
+      color: #999999;
+
+      &:hover {
+        color: #333333;
+        text-decoration: none;
+      }
+    }
+  }
+
+  .socialites {
+    width: 100%;
+    height: auto;
+    float: left;
+    margin-bottom: 20px;
+    margin-top: 15px;
+
+    .title {
+      width: 100%;
+      height: auto;
+      float: left;
       font-size: 14px;
-      color: rgba(0, 0, 0, 0.7);
+      line-height: 20px;
+      color: #999999;
+      margin-bottom: 15px;
+      text-align: center;
     }
   }
 }
-
-.other-box {
-  width: 100%;
-  height: auto;
-  float: left;
-  margin-top: 15px;
-  margin-bottom: 150px;
-  padding-left: 30px;
-  font-size: 14px;
-  color: rgba(0, 0, 0, 0.7);
-}
 </style>
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-4">
-        <div class="login-box">
-          <div class="title">账号登录</div>
-          <div class="sms-login-button">
-            <span @click="switchTab()">{{
-              tab === "sms" ? "密码登录" : "短信登录"
-            }}</span>
-          </div>
-          <template v-if="tab === 'password'">
-            <div class="form-group">
-              <label for="mobile">手机号</label>
-              <input
-                id="mobile"
-                type="mobile"
-                class="form-control"
-                placeholder="手机号"
-                v-model="form.mobile"
-                required
+  <div class="main-body">
+    <div class="logo-box">
+      <nuxt-link to="/">
+        <img src="~assets/images/white-logo.png" width="140" height="37"
+      /></nuxt-link>
+    </div>
+    <div class="login-box">
+      <div class="tabs">
+        <div
+          class="tab-item"
+          @click="tab = 'password'"
+          :class="{ active: tab === 'password' }"
+        >
+          密码登录
+        </div>
+        <div
+          class="tab-item"
+          @click="tab = 'sms'"
+          :class="{ active: tab === 'sms' }"
+        >
+          短信登录
+        </div>
+      </div>
+      <template v-if="tab === 'password'">
+        <div class="form-group">
+          <input
+            type="text"
+            class="input-form-item"
+            placeholder="手机号"
+            v-model="form.mobile"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            class="input-form-item"
+            placeholder="密码"
+            v-model="form.password"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <button
+            type="button"
+            class="auth-button"
+            :class="{
+              active: form.mobile.length === 11 && form.password.length > 0,
+            }"
+            @click="loginHandler"
+            :disabled="!(form.mobile.length === 11 && form.password.length > 0)"
+          >
+            登录
+          </button>
+        </div>
+      </template>
+      <template v-else>
+        <div class="form-group">
+          <input
+            type="text"
+            class="input-form-item"
+            placeholder="手机号"
+            v-model="smsLogin.mobile"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <div class="input-group">
+            <input
+              type="text"
+              name="captcha"
+              placeholder="验证码"
+              class="input-form-item"
+              style="width: 120px; margin-right: 15px"
+              v-model="smsLogin.captchaImage"
+              required
+            />
+            <div class="input-group-append">
+              <img
+                :src="captchaImage.img"
+                @click="switchCaptcha"
+                class="captcha"
+                width="120"
+                height="38"
               />
             </div>
-            <div class="form-group">
-              <label for="password">密码</label>
-              <input
-                id="password"
-                type="password"
-                class="form-control"
-                placeholder="密码"
-                v-model="form.password"
-                required
-              />
-            </div>
-          </template>
-          <template v-else>
-            <div class="form-group">
-              <label>手机号</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="手机号"
-                v-model="smsLogin.mobile"
-                required
-              />
-            </div>
-            <div class="form-group">
-              <label>验证码</label>
-              <div class="row">
-                <div class="col">
-                  <div class="input-group">
-                    <input
-                      type="text"
-                      name="captcha"
-                      placeholder="验证码"
-                      class="form-control"
-                      v-model="smsLogin.captchaImage"
-                      required
-                    />
-                    <div class="input-group-append">
-                      <img
-                        :src="captchaImage.img"
-                        @click="switchCaptcha"
-                        class="captcha"
-                        width="120"
-                        height="38"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
-              <label>手机验证码</label>
-              <div class="row">
-                <div class="col">
-                  <div class="input-group">
-                    <input
-                      type="text"
-                      name="sms_captcha"
-                      placeholder="手机验证码"
-                      class="form-control"
-                      v-model="smsLogin.sms"
-                      required
-                    />
-                    <div class="input-group-append">
-                      <button
-                        type="button"
-                        style="width: 120px"
-                        class="btn btn-primary"
-                        :disabled="sms.loading"
-                        @click="sendSmsCode"
-                      >
-                        <span v-if="sms.loading">{{ sms.current }}s</span>
-                        <span v-else>发送验证码</span>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </template>
-          <div class="form-group">
-            <button
-              type="button"
-              class="btn btn-primary btn-block"
-              @click="loginHandler"
-            >
-              登录
-            </button>
           </div>
         </div>
-        <div class="other-box">
-          还没有账号？<nuxt-link to="/auth/register">立即注册</nuxt-link>
+        <div class="form-group">
+          <div class="input-group">
+            <input
+              type="text"
+              name="sms_captcha"
+              placeholder="手机验证码"
+              style="width: 120px; margin-right: 15px"
+              class="input-form-item"
+              v-model="smsLogin.sms"
+              required
+            />
+            <div class="input-group-append">
+              <button
+                type="button"
+                style="width: 120px; height: 42px"
+                class="send-sms-button"
+                :class="{
+                  active:
+                    sms.loading === false && smsLogin.captchaImage.length > 0,
+                }"
+                :disabled="sms.loading"
+                @click="sendSmsCode"
+              >
+                <span v-if="sms.loading">{{ sms.current }}s</span>
+                <span v-else>发送验证码</span>
+              </button>
+            </div>
+          </div>
         </div>
+
+        <div class="form-group">
+          <button
+            type="button"
+            class="auth-button"
+            :class="{
+              active: smsLogin.mobile.length === 11 && smsLogin.sms.length > 0,
+            }"
+            @click="smsLoginHandler"
+            :disabled="
+              !(smsLogin.mobile.length === 11 && smsLogin.sms.length > 0)
+            "
+          >
+            登录
+          </button>
+        </div>
+      </template>
+      <div class="other-buttons">
+        <nuxt-link to="/auth/password">忘记密码</nuxt-link>
+        &nbsp;|&nbsp;
+        <nuxt-link to="/auth/register">注册</nuxt-link>
+      </div>
+      <div class="socialites">
+        <div class="title">第三方账号登录</div>
+        <div class="apps"></div>
       </div>
     </div>
   </div>
@@ -176,7 +310,6 @@
 
 <script>
 export default {
-  layout: "app",
   data() {
     return {
       tab: "password",
@@ -234,28 +367,7 @@ export default {
     switchTab() {
       this.tab = this.tab === "password" ? "sms" : "password";
     },
-    loginHandler() {
-      if (this.tab === "password") {
-        if (this.form.mobile.length !== 11) {
-          this.$toast.error("请输入手机号");
-          return;
-        }
-        if (this.form.password.length === 0) {
-          this.$toast.error("请输入密码");
-          return;
-        }
-        this.$api.Auth.LoginPassword({
-          mobile: this.form.mobile,
-          password: this.form.password,
-        }).then((res) => {
-          if (res.code !== 0) {
-            this.$toast.error(res.message);
-            return;
-          }
-        });
-        return;
-      }
-
+    smsLoginHandler() {
       if (this.smsLogin.mobile.length !== 11) {
         this.$toast.error("请输入手机号");
         return;
@@ -273,6 +385,28 @@ export default {
           return;
         }
         // 登录成功
+        this.$toast.success("登录成功");
+      });
+    },
+    loginHandler() {
+      if (this.form.mobile.length !== 11) {
+        this.$toast.error("请输入手机号");
+        return;
+      }
+      if (this.form.password.length === 0) {
+        this.$toast.error("请输入密码");
+        return;
+      }
+      this.$api.Auth.LoginPassword({
+        mobile: this.form.mobile,
+        password: this.form.password,
+      }).then((res) => {
+        if (res.code !== 0) {
+          this.$toast.error(res.message);
+          return;
+        }
+
+        this.$toast.success("登录成功");
       });
     },
     sendSmsCode() {
